1a1e44f9e4c0f323065b52b8bd0f95af3e9f54fd
paolavallejo <pvallej3@eafit.edu.co>
2024-04-29 13:46:09 -0500

    Actualización de BD

--- movie/models.py
+++ movie/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Movie(models.Model): 
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250) 
+    description = models.CharField(max_length=1000) 
     image = models.ImageField(upload_to='movie/images/') 
     url = models.URLField(blank=True)
     genre = models.CharField(blank=True, max_length=250)
--- movie/views.py
+++ movie/views.py
@@ -8,6 +8,11 @@ from .models import Movie, Review
 
 from .forms import ReviewForm
 
+import matplotlib.pyplot as plt
+import matplotlib
+import io
+import urllib, base64
+
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
@@ -29,16 +34,50 @@ def signup(request):
     return render(request, 'signup.html', {'email':email})
 
 def statistics(request):
-    # Consulta para obtener películas agrupadas por año
-    movies_by_year = Movie.objects.values('year').annotate(total=Count('id')).order_by('year')
-    
-    # Convertir los resultados de la consulta a una lista de diccionarios
-    movies_by_year_list = [{'year': item['year'], 'total': item['total']} for item in movies_by_year]
-    
-    # Serializar la lista de diccionarios a JSON
-    movies_by_year_json = json.dumps(movies_by_year_list)
-    
-    return render(request, 'statistics.html', {'movies_by_year_json': movies_by_year_json})
+    matplotlib.use('Agg')
+    # Gráfica de películas por año
+    all_movies = Movie.objects.all()
+    movie_counts_by_year = {}
+    for movie in all_movies:
+        year = movie.year if movie.year else "None"
+        if year in movie_counts_by_year:
+            movie_counts_by_year[year] += 1
+        else:
+            movie_counts_by_year[year] = 1
+
+    year_graphic = generate_bar_chart(movie_counts_by_year, 'Year', 'Number of movies')
+
+    # Gráfica de películas por género
+    movie_counts_by_genre = {}
+    for movie in all_movies:
+        # Obtener el primer género
+        genres = movie.genre.split(',')[0].strip() if movie.genre else "None"
+        if genres in movie_counts_by_genre:
+            movie_counts_by_genre[genres] += 1
+        else:
+            movie_counts_by_genre[genres] = 1
+
+    genre_graphic = generate_bar_chart(movie_counts_by_genre, 'Genre', 'Number of movies')
+
+    return render(request, 'statistics.html', {'year_graphic': year_graphic, 'genre_graphic': genre_graphic})
+
+
+def generate_bar_chart(data, xlabel, ylabel):
+    keys = [str(key) for key in data.keys()]
+    plt.bar(keys, data.values())
+    plt.title('Movies Distribution')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
+    image_png = buffer.getvalue()
+    buffer.close()
+    graphic = base64.b64encode(image_png).decode('utf-8')
+    return graphic
 
 def detail(request, movie_id):
     movie = get_object_or_404(Movie,pk=movie_id)

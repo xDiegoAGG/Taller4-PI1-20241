44b6c7aa851041e9f36ab8fd406dae969a0fcf51
paolavallejo <pvallej3@eafit.edu.co>
2024-04-29 12:45:00 -0500

    authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.forms import UserCreationForm
+from .forms import UserCreateForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
@@ -10,7 +11,8 @@ from django.db import IntegrityError
 def signupaccount(request):
     if request.method == 'GET':
         return render(request, 'signupaccount.html',
-                    {'form':UserCreationForm})
+                    #{'form':UserCreationForm})
+                    {'form':UserCreateForm})
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
@@ -21,11 +23,13 @@ def signupaccount(request):
                 return redirect('home')
             except IntegrityError:
                 return render(request, 'signupaccount.html',
-                    {'form':UserCreationForm,
+                    #{'form':UserCreationForm,
+                    {'form':UserCreateForm,
                     'error':'Username already taken. Choose new username.'})
         else:
             return render(request, 'signupaccount.html',
-                {'form':UserCreationForm, 'error':'Passwords do not match'})
+                #{'form':UserCreationForm, 'error':'Passwords do not match'})
+                {'form':UserCreateForm, 'error':'Passwords do not match'})
 
 
 def logoutaccount(request):
--- movie/views.py
+++ movie/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Count
 import json
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 
-from .models import Movie
+from .models import Movie, Review
+
+from .forms import ReviewForm
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
@@ -41,3 +43,18 @@ def statistics(request):
 def detail(request, movie_id):
     movie = get_object_or_404(Movie,pk=movie_id)
     return render(request, 'detail.html', {'movie':movie})
+
+def createreview(request, movie_id): 
+    movie = get_object_or_404(Movie,pk=movie_id) 
+    if request.method == 'GET': 
+        return render(request, 'createreview.html', {'form':ReviewForm(), 'movie': movie}) 
+    else: 
+        try: 
+            form = ReviewForm(request.POST)
+            newReview = form.save(commit=False) 
+            newReview.user = request.user 
+            newReview.movie = movie 
+            newReview.save() 
+            return redirect('detail', newReview.movie.id) 
+        except ValueError: 
+            return render(request, 'createreview.html', {'form':ReviewForm(),'error':'bad data passed in'})

b2f98e4ff7ba9f722695033305447214da278db0
paolavallejo <pvallej3@eafit.edu.co>
2024-04-30 10:46:45 -0500

    update reviews

--- movie/views.py
+++ movie/views.py
@@ -100,3 +100,16 @@ def createreview(request, movie_id):
             return redirect('detail', newReview.movie.id) 
         except ValueError: 
             return render(request, 'createreview.html', {'form':ReviewForm(),'error':'bad data passed in'})
+        
+def updatereview(request, review_id): 
+    review = get_object_or_404(Review,pk=review_id,user=request.user) 
+    if request.method =='GET': 
+        form = ReviewForm(instance=review) 
+        return render(request, 'updatereview.html', {'review': review,'form':form}) 
+    else: 
+        try: 
+            form = ReviewForm(request.POST, instance=review) 
+            form.save() 
+            return redirect('detail', review.movie.id) 
+        except ValueError: 
+            return render(request, 'updatereview.html', {'review': review,'form':form,'error':'Bad data in form'})

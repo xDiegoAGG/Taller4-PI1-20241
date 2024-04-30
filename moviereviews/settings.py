e9a8befbd85e77459381be486a5844237fe67933
paolavallejo <pvallej3@eafit.edu.co>
2024-04-30 13:54:56 -0500

    nueva ip externa

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-+9jg)*p09$at+zck$n-6*djf=34w4sc*m##^fmph4pb$_nh%xb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.27.40.134']
+ALLOWED_HOSTS = ['35.238.32.154']
 
 
 # Application definition

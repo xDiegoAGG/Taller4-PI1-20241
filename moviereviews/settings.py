d8a2e65334f2a33670fdad842c7dc200f8de6fc1
paolavallejo <pvallej3@eafit.edu.co>
2024-04-30 10:57:51 -0500

    database update

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -131,7 +132,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
- os.path.join(BASE_DIR, "static"),
+ #os.path.join(BASE_DIR, "static"),
  'moviereviews/static/',
 ]
 
